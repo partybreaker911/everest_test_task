762f8df14ee699d872f5593a6ef8d286d443a1b1
To Kirill <tio.kirill@gmail.com>
2023-07-27 16:00:19 +0300

    worker setup

--- app/users/__init__.py
+++ app/users/__init__.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 
-users_blueprint = Blueprint("users", __name__, url_prefix="/users", \
-        template_folder="templates")
+users_blueprint = Blueprint(
+    "users", __name__, url_prefix="/users", template_folder="templates"
+)
 
-from . import models # noqa
+from . import models, tasks  # noqa
